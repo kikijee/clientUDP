8311fe770571b49958cc2b641a34bad43c9e1131
Criptian TrainToBusan <cam007mani@gmail.com>
2022-11-04 20:18:57 -0700

    server name change

--- UDPClient.py
+++ UDPClient.py
@@ -1,6 +1,6 @@
 from socket import *
-serverName = 'localhost'
-serverPort = 18000
+serverName = '192.168.1.248'
+serverPort = 18111
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = input('Input lowercase sentence:')
 clientSocket.sendto(message.encode(),(serverName, serverPort))
